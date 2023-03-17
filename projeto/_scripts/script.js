fee2a8a15d82ac6379a73b93773dc945e9f09621
Felipe-Bignoto <felipebignoto@gmail.com>
2023-03-17 15:07:50 -0300

    iniciar aula 82

--- projeto/_scripts/script.js
+++ projeto/_scripts/script.js
@@ -1,8 +1,20 @@
 var quantidade = $('div#testemunhos ul').length;
 var limite = (quantidade * 336) * -1;
 var largura = limite + 336;
-var view_largura = $('section#testemunha div#testemunho-centro').width();
+var view_largura = $('section#testemunho div#testemunho-centro').width();
 
 $('div#testemunho').css('width',limite*-1);
 
 $('div#teste').html(limite);
+
+
+$('div#testemunho-direita').click(function(){
+    posicao_atual = $('div#testemunhos').position().left;
+    nova_posicao = posicao_atual - view_largura;
+
+    $('div#testemunhos').animate({left:nova_posicao},1000);
+})
+
+$('div#testemunho-esquerda').click(function(){
+    $('div#teste').html("Clicou na esquerda");
+})
