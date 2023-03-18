16815ff55638c9d861ec1aec8b423bc83d11ff4b
Felipe-Bignoto <felipebignoto@gmail.com>
2023-03-18 09:47:05 -0300

    final da seçãi 12

--- projeto/_scripts/script.js
+++ projeto/_scripts/script.js
@@ -5,16 +5,40 @@ var view_largura = $('section#testemunho div#testemunho-centro').width();
 
 $('div#testemunho').css('width',limite*-1);
 
-$('div#teste').html(limite);
+
 
 
 $('div#testemunho-direita').click(function(){
     posicao_atual = $('div#testemunhos').position().left;
     nova_posicao = posicao_atual - view_largura;
 
-    $('div#testemunhos').animate({left:nova_posicao},1000);
+    if(limite < (posicao_atual - view_largura)){
+        $('div#testemunhos').animate({left:nova_posicao},1000);
+    }
+
+    if(limite > (nova_posicao - view_largura)){
+        $('div#testemunho-direita ion-icon').css('opacity',0.5);
+        $('div#testemunho-direita ion-icon').css('cursor','not-allowed');
+    }
+
+    $('div#testemunho-esquerda ion-icon').css('opacity',1);
+    $('div#testemunho-esquerda ion-icon').css('cursor','pointer');
+    
 })
 
 $('div#testemunho-esquerda').click(function(){
-    $('div#teste').html("Clicou na esquerda");
+    posicao_atual = $('div#testemunhos').position().left;
+
+    if(posicao_atual + view_largura <= 0){
+        nova_posicao = posicao_atual + view_largura;
+        $('div#testemunhos').animate({left:nova_posicao},1000);
+    }
+
+    if(nova_posicao == 0){
+        $('div#testemunho-esquerda ion-icon').css('opacity',0.5);
+        $('div#testemunho-esquerda ion-icon').css('cursor','not-allowed');
+    }
+
+    $('div#testemunho-direita ion-icon').css('opacity',1);
+    $('div#testemunho-direita ion-icon').css('cursor','pointer');
 })
